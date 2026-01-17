4e6dfd4e8031699c7e77c454d66e49eba89dcc37
kundan Suthar <kundanss22@gmail.com>
2026-01-17 10:31:27 +0530

    remove dupplicate from array solved

---
+++ arrays/remove_duplicate/solution.js
@@ -0,0 +1,12 @@
+function removeDuplicates(nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < nums[x]) {
+      nums[x] = nums[i];
+    }
+  }
+  return { nums, x };
+}
+
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+console.log(removeDuplicates(nums));
